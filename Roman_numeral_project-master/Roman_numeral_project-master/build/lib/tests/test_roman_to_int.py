27a0737e9b9d4d58f7aa46957b3f51e75a349229
James Smith <jamessmith@projectingsuccess.co.uk>
2022-05-06 14:00:32 +0100

    roman numeral project

---
+++ Roman_numeral_project-master/Roman_numeral_project-master/build/lib/tests/test_roman_to_int.py
@@ -0,0 +1,3 @@
+from roman_to_int import roman_to_int
+def test_roman_to_int():
+    assert roman_to_int.roman_to_int("IV" == 4)
---
+++ Roman_numeral_project-master/Roman_numeral_project-master/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="roman_to_int",
+    version="0.0.5",
+    author="James",
+    description="An aplication that converts roman numerals to integers",
+    packages=find_packages(),
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    
+)
