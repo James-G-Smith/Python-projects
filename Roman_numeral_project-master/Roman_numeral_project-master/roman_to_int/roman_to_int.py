87997007f3879df65c1d633050bf5be0b1a73778
James Smith <jamessmith@projectingsuccess.co.uk>
2022-05-07 13:14:55 +0100

    roman numeral project

--- Roman_numeral_project-master/Roman_numeral_project-master/roman_to_int/roman_to_int.py
+++ Roman_numeral_project-master/Roman_numeral_project-master/roman_to_int/roman_to_int.py
@@ -1,9 +1,12 @@
 class roman_tools:
     def roman_to_int(self,r):
+        #dictionary containing roman numerals
         rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        #start the int_val at 0
         int_val = 0
-        r=str(r)
+        #for loop to iterate through roman numeral
         for i in range(len(r)):
+            #if statement to decide if vales are bigger than each other
             if i > 0 and rom_val[r[i]] > rom_val[r[i - 1]]:
                 int_val += rom_val[r[i]] - 2 * rom_val[r[i - 1]]
             else:
